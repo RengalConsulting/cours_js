97047030abd1dfc97bdcb9944772515077f45ea1
Neo <alex.atc.digital@gmail.com>
2020-02-10 20:06:46 +0100

    Fiche resumé cours JS jour 1

---
+++ main.js
@@ -0,0 +1,30 @@
+//var num = 7;
+
+// incrémentation num = num + 1; ( num-- )
+
+// num++;
+
+/* camelCase : privilégie variable explicite (anglais), 
+1er mot minuscule */
+
+//var exempleConcret = 0;
+
+
+/* Concaténation de string */
+
+var hello = "hello";
+var nom = "Al";
+
+hello += " " + nom;
+
+console.log(hello);
+
+
+/* trouver la taille d'une string / lettre selon index / optimisé 
+ -  console.log(variable.length);
+ -  console.log(variable[2]);
+ -  console.log(variable[variable.length - 2]);
+
+*/
+
+// 11.02.2020 Array et boucle
